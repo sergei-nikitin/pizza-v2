320adc83167b20a99d5612a4f20a2bbd19211388
sergei-nikitin <s.dm.nikitin@gmail.com>
2022-09-21 17:43:59 +0300

    21

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { store } from './redux/store';
 
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <Provider store={store}>
     <BrowserRouter>
@@ -15,3 +19,4 @@ root.render(
     </BrowserRouter>
   </Provider>,
 );
+}
--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -2,8 +2,12 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 
-const FullPizza = () => {
-  const [pizza, setPizza] = useState();
+const FullPizza: React.FC = () => {
+  const [pizza, setPizza] = useState<{
+    imageUrl: string;
+    name: string;
+    price: number;
+  }>();
   const { id } = useParams();
   const navigate = useNavigate();
 
@@ -26,7 +30,7 @@ const FullPizza = () => {
   }, []);
 
   if (!pizza) {
-    return 'Загрузка...';
+    return <>'Загрузка...'</>;
   }
 
   return (
